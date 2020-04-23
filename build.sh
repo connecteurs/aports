3606bf58dccd7c423ddcdd4dcad22247b6a5fcaf
ludovicm67 <ludovicmuller1@gmail.com>
2020-04-23 23:30:47 +0200

    cyrus-imapd: build using gcc

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run --rm -P -it \
+  -v$(pwd)/${2:-main}:/home/builder/aports \
+  --workdir /home/builder/aports/${1:-cyrus-sasl} \
+  --name docker-abuild alpinelinux/docker-abuild -r
