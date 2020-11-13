c33100d37ba063f352988cf2df3b9d8cdd4f3fd6
Ludovic Muller <ludovic@connecteu.rs>
2020-11-14 00:41:11 +0100

    use our abuild Docker image

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker run --rm -P -it \
   -v$(pwd)/${2:-main}:/home/builder/aports \
   --workdir /home/builder/aports/${1:-cyrus-sasl} \
-  --name docker-abuild alpinelinux/docker-abuild -r
+  --name docker-abuild registry.gitlab.com/les-connecteurs/docker/abuild -r
